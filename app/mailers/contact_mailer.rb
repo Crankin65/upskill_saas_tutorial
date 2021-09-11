3045afbfb5f9303605ff3d9bc6b070fb8f826ac8
Cameron Rankin <crankin65@gmail.com>
2021-09-11 20:05:06 +0000

    Added mailer settings to environment file and updated email address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'kardnl.direction@gmail.com'  
+  default to: 'djcardnl@gmail.com'  
   
   def contact_email(name, email, body)
     @name = name
